bc97b63edbebfe7bb68cf88f5404e8979249326f
Randall Tanaka <56574248+RanTanaka@users.noreply.github.com>
2020-02-25 12:14:50 -1000

    Cleaned up second-screen.component.ts and html files.
    This should leave us with a clean slate to start experimenting on.

--- src/app/second-screen/second-screen.component.ts
+++ src/app/second-screen/second-screen.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MultiWindowService, Message} from 'ngx-multi-window';
 //import { Plan } from '../interfaces/plan';
-import { OahuPlan } from '../../assets/plans/oahu/plan';
-import { MauiPlan } from '../../assets/plans/maui/plan';
+// import { OahuPlan } from '../../assets/plans/oahu/plan';
+// import { MauiPlan } from '../../assets/plans/maui/plan';
 import { BigIslandPlan } from '../../assets/plans/bigisland/plan';
 import { Plan } from '@app/interfaces';
 
@@ -19,20 +19,29 @@ import { Plan } from '@app/interfaces';
  */
 export class SecondScreenComponent implements OnInit {
 
-  private currentYear: number;  
-  private displayName: string;  
+  // private currentYear: number;  
+  // private displayName: string;  
+  // private nextLayer: string;  
   private secondScreenImagePath: string;   
-  private nextLayer: string;  
   private plan: Plan;
-  private mapLayers: {text: string, color: string, active: boolean}[] = [];
+  // private mapLayers: {text: string, color: string, active: boolean}[] = [];
 
   constructor(private multiWindowService: MultiWindowService) {
     multiWindowService.name = 'secondScreen';
   }
 
+/** Runs when the component is initialized.  Catches a Message and calls an 
+ * appropiate function depending on which Message is caught.
+ * For initialization, 'setup' should be caught.  
+ * Updates can have other values for Message.
+ */
+
   ngOnInit() {
     this.multiWindowService.onMessage().subscribe((value: Message) => {
-        //TODO: Add what to do on initialization here.
+      const data = JSON.parse(value.data);
+      if (data.type === 'setup') {
+        this.setupSecondScreen(data);
+      } 
     });
   }
 
@@ -45,7 +54,6 @@ export class SecondScreenComponent implements OnInit {
    * @param data => The setup object
    */
   private setupSecondScreen(data: any): void {
-    this.currentYear = data.currentYear;
     switch (data.name) {
       case 'bigisland':
         this.plan = BigIslandPlan;
@@ -54,10 +62,6 @@ export class SecondScreenComponent implements OnInit {
         this.plan = BigIslandPlan;
         break;
     }
-    this.nextLayer = this.plan.map.mapLayers[0].name;
-    this.plan.map.mapLayers.forEach(layer => {
-      this.mapLayers.push({text: layer.displayName, color: layer.fillColor, active: false});
-    });
+    
+    };
   }
-
-}
