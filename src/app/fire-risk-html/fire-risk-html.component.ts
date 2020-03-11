a2fdbb4b30276dfcaf06efcbf4a2ded7425163d7
Randall Tanaka <56574248+RanTanaka@users.noreply.github.com>
2020-03-11 11:24:45 -1000

    Created a component for the Fire Risk Layer HTML file. If things go right, we should be able to feed components into an HTML without worrying about divs and stuff.

---
+++ src/app/fire-risk-html/fire-risk-html.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fire-risk-html',
+  templateUrl: './fire-risk-html.component.html',
+  styleUrls: ['./fire-risk-html.component.css']
+})
+export class FireRiskHtmlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
