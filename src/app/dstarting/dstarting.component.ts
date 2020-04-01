e5d1b364a5e4fa83eb3248568671fbb6a24af857
RussMasu <masuda3@hawaii.edu>
2020-03-31 19:29:10 -1000

    Added hmtl component for second screen

---
+++ src/app/dstarting/dstarting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dstarting',
+  templateUrl: './dstarting.component.html',
+  styleUrls: ['./dstarting.component.css']
+})
+export class DstartingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
